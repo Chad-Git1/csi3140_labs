f73e642544fd4d694affb40752cb3b66f309f7f3
Chad <24cyassin@gmail.com>
2024-06-07 12:31:41 -0400

    finished game

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,3 +1,10 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 function createGame(n) {
 
     let array = Array(n).fill(".");
@@ -20,7 +27,7 @@ function createGame(n) {
     array[ghost] = "^."
     array[pacman] = "C"
 
-    let maxScore = 50 + (n - 3)*10
+    let maxScore = 50 + (n - 2)*10
     
     let game = [array,[pacman,ghost],0,maxScore,n];
     return game;
@@ -50,36 +57,131 @@ function moveRight(game){
     return processGame(game,x);  
 }
 
+function moveGhost(game){
+    let r = Math.floor(Math.random() * 4);
+    if(r === 1){
+        if(game[0][game[1][1]] === "^."){
+            game[0][game[1][1]] = ".";
+        } else if(game[0][game[1][1]] === "^@"){
+            game[0][game[1][1]] = "@";
+        } else{
+            game[0][game[1][1]] = "";
+        }
+
+        if (game[1][1] === 0) {
+            game[1][1] = game[0].length - 1;
+        } else {
+            game[1][1] -= 1;
+        }
+
+        if(game[0][game[1][1]] === "."){
+            game[0][game[1][1]] = "^.";
+        } else if(game[0][game[1][1]] === "@"){
+            game[0][game[1][1]] = "^@";
+        } else{
+            game[0][game[1][1]] = "^";
+        }
+
+    } else if (r === 2){
+
+        if(game[0][game[1][1]] === "^."){
+            game[0][game[1][1]] = ".";
+        } else if(game[0][game[1][1]] === "^@"){
+            game[0][game[1][1]] = "@";
+        } else{
+            game[0][game[1][1]] = "";
+        }
+
+        if (game[1][1] === game[0].length - 1) {
+            game[1][1] = 0;
+        } else {
+            game[1][1] += 1;
+        }
+
+        if(game[0][game[1][1]] === "."){
+            game[0][game[1][1]] = "^.";
+        } else if(game[0][game[1][1]] === "@"){
+            game[0][game[1][1]] = "^@";
+        } else{
+            game[0][game[1][1]] = "^";
+        }
+    }
+    return game;
+}
+
 function processGame(game,x){
 
-    /*if (game[1][0] === game[1][1]){
+    game[0][x] = "";
+    game = moveGhost(game);
+    if (game[1][0] === game[1][1]){
         return("game over | eaten by ghost | score = " + game[2]);
     }
-    else{*/
-        game[0][x] = "";
+    else if (game[0][game[1][0]] === "."){
+        game[0][game[1][0]] = "C";
+        game[2] += 10;
+        console.log(game[2])
+        console.log(game[3])
         if (game[2] === game[3]){
             console.log("Level completed! score = " + game[2]);
-            return newLevel(game);
+            return newLevel(game);  
         }
-        else if (game[0][game[1][0]] === "."){
-            game[0][game[1][0]] = "C";
-            game[2] += 10;
-            return game;
-        }
-        else if (game[0][game[1][0]] === "@"){
-            game[0][game[1][0]] = "C";
-            game[2] += 50;
-            return game;
-        }
-        game[0][game[1][0]] = "C";
         return game;
     }
-//}
+    else if (game[0][game[1][0]] === "@"){
+        game[0][game[1][0]] = "C";
+        game[2] += 50;
+        if (game[2] === game[3]){
+            console.log("Level completed! score = " + game[2]);
+            return newLevel(game);  
+        }
+        return game;
+    }
+    game[0][game[1][0]] = "C";
+    return game;
+}
 
 function newLevel(game){
     let score = game[2];
-    game = createGame(game[4]);
-    game[3] += score;
-    game[2] = score;
-    return game;
+    newGame = createGame(game[4]);
+    newGame[3] = game[3] + score;
+    newGame[2] = score;
+    return newGame;
 }
+
+function main() {
+    rl.question("Enter size of game: ", function(size) {
+        let n = parseInt(size);
+        let game = createGame(n);
+
+        playGame(game);
+    });
+}
+
+function playGame(game) {
+    console.log("Current game:");
+    console.log(game[0].join(' '));
+    console.log("Score: " + game[2]);
+
+    rl.question("Move Pacman left or right? (L/R): ", function(direction) {
+        if (direction.toUpperCase() === "L") {
+            game = moveLeft(game);
+        } else if (direction.toUpperCase() === "R") {
+            game = moveRight(game);
+        } else {
+            console.log("Invalid input. Please enter 'L' to move left or 'R' to move right.");
+            playGame(game);
+            return; 
+        }
+
+        if (typeof game === 'string') {
+            console.log(game);
+            rl.close();
+        } else {
+            playGame(game);
+        }
+    });
+}
+
+
+main();
+
